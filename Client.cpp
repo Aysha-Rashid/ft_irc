4d664e5cf133c2720f43489d42c1ec1dc48e47d2
EmyCodez <130977482+EmyCodez@users.noreply.github.com>
2025-02-18 15:59:07 +0400

    completed client hpp

--- Client.cpp
+++ Client.cpp
@@ -1,9 +1,14 @@
 # include "Client.hpp"
 
-Client::Client(/* args */)
+Client::Client() {}
+
+Client::Client(int fd,std::string &ip, std::string  &hostname) :_fd(fd), _ipAddress(ip),_hostname(hostname),_channelCount(0) {}
+
+Client::Client(const Client &other){}
+
+Client& Client::operator=(const Client &other)
 {
+
 }
 
-Client::~Client()
-{
-}
+Client::~Client() {}
--- Client.hpp
+++ Client.hpp
@@ -15,13 +15,14 @@ class Client
 {
 private:
     int _fd;
-    std::string _ipAddr;
+    std::string _ipAddress;
     std::string _hostname;
     std::string _username;
     std::string _nickname;
     std::string _realname;
     clientState  _state;
     int _channelCount;
+    
     Client();  
     Client(const Client &other);
     Client &operator = (const Client &other);
@@ -36,13 +37,19 @@ public:
     std::string getUserName(void)   const;
     std::string getRealName(void)   const;
     std::string getNickName(void)   const;
+    std::string getIpAddress(void) const;
     clientState getState(void)  const;
     int getChannelCount(void)   const;
 
     //setters
-    void    setHostName()
+    void    setHostName(std::string &hostname);
+    void    setUserName(std::string &username);
+    void    setRealName(std::string &realname);
+    void    setNickName(std::string &nickname);
+    void    setState(clientState state);
+
     //member    functions
-    void    sendMessage(const   std::string msg)    const;
+    void    write(const   std::string msg)    const;
 };
 
 # endif
