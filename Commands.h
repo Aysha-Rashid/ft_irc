62d8ad4d40bcb64efe7b08dfece5dad1130bcb0a
Salam Sibai <salam.sibai@hotmail.com>
2025-03-10 09:40:25 -0700

    wip

--- Commands.h
+++ Commands.h
@@ -12,8 +12,8 @@ public:
 	Commands&	operator=(Commands& rhs) = delete;
 	~Commands() = delete;
 
-	static void kick(const Client& client /* channel to kick from, client to kick*/);
-	static void invite(const Client& admin, const Client& invitee);
+	static void kick(const Client& admin, const Client& client /* channel to kick from*/);
+	static void invite(const Client& admin, const Client& invitee /*, channel to invite to*/);
 	static void topic(/**/);
 	static void mode(/**/);
 };
